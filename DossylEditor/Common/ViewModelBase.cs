65205779ea0bff3eb132b35f604dc7e763c945d9
Alex Wissing <awissing123@gmail.com>
2023-06-02 18:51:16 -0500

    vid 2. Created folder for project templates and load them in to populate new project view.

---
+++ DossylEditor/Common/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DossylEditor {
+    public class ViewModelBase : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DossylEditor/GameProject/NewProject.cs
@@ -0,0 +1,77 @@
+﻿using DossylEditor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DossylEditor.GameProject {
+    [DataContract]
+    public class ProjectTemplate {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+
+        public byte[] Icon { get; set; }
+
+        public byte[] Screenshot { get; set; }
+        public string IconFilePath { get; set; }
+        public string ScreenshotFilePath { get; set; }
+        public string ProjectFilePath { get; set; }
+    }
+    class NewProject : ViewModelBase {
+        // TODO: get installation path
+        private readonly string _templatePath = @"..\..\DossylEditor\ProjectTemplates";
+        private string _projectName = "NewProject";
+        public string ProjectName {
+            get => _projectName;
+            set {
+                if (_projectName != value) {
+                    _projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
+                }
+            }
+        }
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\DossylProjects\";
+        public string ProjectPath {
+            get => _projectPath;
+            set {
+                if (_projectPath != value) {
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
+
+        public NewProject() {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+            try {
+                var templatesFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templatesFiles.Any());
+                foreach (var file in templatesFiles) {
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+                    _projectTemplates.Add(template);
+                }
+            } catch (Exception ex) {
+                Debug.WriteLine(ex.Message);
+                // TODO: log errors
+            }
+        }
+    }
+}
